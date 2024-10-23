58bf68b16d297108c89eca8c880058feb239e1e2
kagon <kagon@DESKTOP-5LS5HTJ>
2024-10-23 15:08:34 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_2
+{
+    public class BankAccount
+    {
+        private string person;
+        private int funds;
+
+        public string Person
+        {
+            get { return person; }
+            set { person = value; }
+        }
+
+        public int Funds
+        {
+            get { return funds; }
+            set { funds = value; }
+        }
+
+        public BankAccount(string person, int funds)
+        {
+            this.person = person;
+            this.funds = funds;
+        }
+
+        public void Wplata()
+        {
+            Console.WriteLine("Ile chcesz wplacic");
+            int wplata = Convert.ToInt32(Console.ReadLine());
+
+            funds = funds + wplata;
+            Console.WriteLine(funds);
+        }
+
+        public void Wyplata()
+        {
+            Console.WriteLine("Ile chcesz wyplacic");
+            int wyplata = Convert.ToInt32(Console.ReadLine());
+
+            if(wyplata > funds)
+            {
+                Console.WriteLine("niewystarczające środki");
+            }
+            else
+            {
+                funds = funds-wyplata;
+                Console.WriteLine(funds);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Laboratorium_2;
+
+BankAccount konto1 = new BankAccount("kacper", 100);
+konto1.Wyplata();
+Console.WriteLine(konto1.Funds);
