5528f850d504df13d82d484da76c5a32a67458b8
kagon <kagon@DESKTOP-5LS5HTJ>
2024-11-01 22:46:38 +0100

    zadanie 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Laboratorium_2;
 
-BankAccount konto1 = new BankAccount("kacper", 100);
-konto1.Wyplata();
-Console.WriteLine(konto1.Funds);
+//BankAccount konto1 = new BankAccount("kacper", 100);
+//konto1.Wyplata();
+//Console.WriteLine(konto1.Funds);
+
+Student student1 = new Student("kacper", "gonc", new[] { 3, 2, 5 });
+student1.AddGrade();
---
+++ Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_2
+{
+    public class Student
+    {
+        private string first_name;
+        private string last_name;
+        private int[] grades {  get; set; }
+
+        public Student(string first_name, string last_name, int[] grades)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.grades = grades;
+        }
+
+        public void View()
+        {
+            foreach (var grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        public void Average()
+        {
+            Console.WriteLine(grades.Average());
+        }
+
+        public void AddGrade() 
+        {
+            Console.WriteLine("Podaj ocene do dodania");
+            int newGrade = Convert.ToInt32(Console.ReadLine());
+
+            grades = [..grades, newGrade];
+
+            foreach (var grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
