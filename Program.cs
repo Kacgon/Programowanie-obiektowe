ac633b4f2db34882f3ba71a9018ffdd0cb0f1d75
kagon <kagon@DESKTOP-5LS5HTJ>
2024-11-24 22:16:45 +0100

    Zadanie 2

--- Program.cs
+++ Program.cs
@@ -2,60 +2,109 @@
 using System;
 using System.Drawing;
 
+//static void Kalkulator()
+//{
+//    Console.WriteLine("Podaj liczbe 1");
+//    int liczba1 = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("podaj liczbe2");
+//    int liczba2 = Convert.ToInt32(Console.ReadLine());
 
-static void Kalkulator()
-{
-    Console.WriteLine("Podaj liczbe 1");
-    int liczba1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("podaj liczbe2");
-    int liczba2 = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("wybierz operacje +.1  -.2  *.3  /.4");
-    int wybrana = Convert.ToInt32(Console.ReadLine());
-    Array wartosci = Enum.GetValues(typeof(operacjeMatematyczne));
-    operacjeMatematyczne costam = (operacjeMatematyczne)wartosci.GetValue(wybrana-1);
+//Console.WriteLine("wybierz operacje +.1  -.2  *.3  /.4");
+//int wybrana = Convert.ToInt32(Console.ReadLine());
+//Array wartosci = Enum.GetValues(typeof(operacjeMatematyczne));
+//operacjeMatematyczne costam = (operacjeMatematyczne)wartosci.GetValue(wybrana - 1);
 
 
-    try
-    {
-        int efektOperacji = ObliczDzialanie(costam, liczba1, liczba2);
+//    try
+//    {
+//        int efektOperacji = ObliczDzialanie(costam, liczba1, liczba2);
 
-        Console.WriteLine(efektOperacji);
-    }
-    catch (DivideByZeroException ex)
-    {
-        Console.WriteLine($"nie dziel przez 0: {ex.Message}");
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("podales string a nie liczbe");
-    }
-    finally {
-    Console.WriteLine("koncowy wynik");
-    }
-}
+//        Console.WriteLine(efektOperacji);
+//    }
+//    catch (DivideByZeroException ex)
+//    {
+//        Console.WriteLine($"nie dziel przez 0: {ex.Message}");
+//    }
+//    catch (FormatException)
+//    {
+//        Console.WriteLine("podales string a nie liczbe");
+//    }
+//    finally {
+//    Console.WriteLine("koncowy wynik");
+//    }
+//}
 
-static int ObliczDzialanie(operacjeMatematyczne operacja, int liczba1, int liczba2)
-{
+//static int ObliczDzialanie(operacjeMatematyczne operacja, int liczba1, int liczba2)
+//{
 
-        switch (operacja)
-        {
+//        switch (operacja)
+//        {
 
-            case operacjeMatematyczne.dodawanie:
-                return liczba1 + liczba2;
-            case operacjeMatematyczne.odejmowanie:
-                return liczba1 - liczba2;
-            case operacjeMatematyczne.mnozenie:
-                return liczba1 * liczba2;
-            case operacjeMatematyczne.dzielenie:
-                return liczba1 / liczba2;
-            default:
-                throw new ArgumentOutOfRangeException();
+//            case operacjeMatematyczne.dodawanie:
+//                return liczba1 + liczba2;
+//            case operacjeMatematyczne.odejmowanie:
+//                return liczba1 - liczba2;
+//            case operacjeMatematyczne.mnozenie:
+//                return liczba1 * liczba2;
+//            case operacjeMatematyczne.dzielenie:
+//                return liczba1 / liczba2;
+//            default:
+//                throw new ArgumentOutOfRangeException();
 
-        }
-    
-}
-Kalkulator();
+//        }
+
+//}
+//Kalkulator();
 
 
 //zad 2
+
+Dictionary<int,(StatusZamowienia, List<string>)> zamowienia = new Dictionary<int, (StatusZamowienia, List<string>)>();
+zamowienia[0] =  (StatusZamowienia.Zrealizowane, new List<string> { "Telewizor"});
+zamowienia[1] = (StatusZamowienia.Oczekujące, new List<string> { "Telefon", "Myszka", "Xbox" });
+zamowienia[2] = (StatusZamowienia.Przyjęte, new List<string> { "Pralka", "Lampa" });
+
+static void ChangeStatus(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
+{
+    Console.WriteLine("Jakie zamowienie chcesz zaktualizowac");
+    int wybrane = Convert.ToInt32(Console.ReadLine());
+
+    try
+    {
+        Console.WriteLine(zamowienia[wybrane].Item1);
+        Console.WriteLine("na jaki status chcesz zmienic :1 oczekujace, 2 przyjete, 3 zrealizowane, 4 anulowane");
+        int wybrana = Convert.ToInt32(Console.ReadLine());
+        Array statusy = Enum.GetValues(typeof(StatusZamowienia));
+        StatusZamowienia zaktualizowanyStatus = (StatusZamowienia)statusy.GetValue(wybrana - 1);
+
+        zamowienia[wybrane] = (zaktualizowanyStatus, zamowienia[wybrane].Item2);
+        Console.WriteLine(zamowienia[wybrane].Item1);
+
+    } 
+    catch (KeyNotFoundException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message );
+    }
+   
+}
+
+
+static void ShowOrders(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
+{
+    foreach (var przedmiot in zamowienia)
+    {
+        Console.WriteLine($"Id: {przedmiot.Key}");
+        Console.WriteLine($"Status zamowienia: {przedmiot.Value.Item1}");
+        Console.WriteLine("Przedmioty:");
+        foreach (var fruit in przedmiot.Value.Item2) 
+        {
+            Console.WriteLine($"- {fruit}");
+        }
+    }
+}
+ShowOrders(zamowienia);
+ChangeStatus(zamowienia);
---
+++ StatusZamowienia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_OOP_5
+{
+    public enum StatusZamowienia
+    {
+        Oczekujące = 1,
+        Przyjęte = 2,
+        Zrealizowane = 3,
+        Anulowane = 4,
+    };
+}
