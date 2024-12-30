38d7376539c55f4513a7e0185cf71e2557b0eb3b
kagon <kagon@DESKTOP-5LS5HTJ>
2024-12-30 16:36:03 +0100

    Uzupełnione zadania lab 5

---
+++ Kolory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_OOP_5
+{
+    public enum Kolory
+    {
+            Czerwony = 1,
+            Niebieski = 2,
+            Zielony = 3,
+            Żółty = 4,
+            Fioletowy = 5,
+    }
+}
--- Program.cs
+++ Program.cs
@@ -59,52 +59,95 @@ using System.Drawing;
 
 //zad 2
 
-Dictionary<int,(StatusZamowienia, List<string>)> zamowienia = new Dictionary<int, (StatusZamowienia, List<string>)>();
-zamowienia[0] =  (StatusZamowienia.Zrealizowane, new List<string> { "Telewizor"});
-zamowienia[1] = (StatusZamowienia.Oczekujące, new List<string> { "Telefon", "Myszka", "Xbox" });
-zamowienia[2] = (StatusZamowienia.Przyjęte, new List<string> { "Pralka", "Lampa" });
+//Dictionary<int,(StatusZamowienia, List<string>)> zamowienia = new Dictionary<int, (StatusZamowienia, List<string>)>();
+//zamowienia[0] =  (StatusZamowienia.Zrealizowane, new List<string> { "Telewizor"});
+//zamowienia[1] = (StatusZamowienia.Oczekujące, new List<string> { "Telefon", "Myszka", "Xbox" });
+//zamowienia[2] = (StatusZamowienia.Przyjęte, new List<string> { "Pralka", "Lampa" });
 
-static void ChangeStatus(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
-{
-    Console.WriteLine("Jakie zamowienie chcesz zaktualizowac");
-    int wybrane = Convert.ToInt32(Console.ReadLine());
+//static void ChangeStatus(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
+//{
+//    Console.WriteLine("Jakie zamowienie chcesz zaktualizowac");
+//    int wybrane = Convert.ToInt32(Console.ReadLine());
 
-    try
-    {
-        Console.WriteLine(zamowienia[wybrane].Item1);
-        Console.WriteLine("na jaki status chcesz zmienic :1 oczekujace, 2 przyjete, 3 zrealizowane, 4 anulowane");
-        int wybrana = Convert.ToInt32(Console.ReadLine());
-        Array statusy = Enum.GetValues(typeof(StatusZamowienia));
-        StatusZamowienia zaktualizowanyStatus = (StatusZamowienia)statusy.GetValue(wybrana - 1);
+//    try
+//    {
+//        Console.WriteLine(zamowienia[wybrane].Item1);
+//        Console.WriteLine("na jaki status chcesz zmienic :1 oczekujace, 2 przyjete, 3 zrealizowane, 4 anulowane");
+//        int wybrana = Convert.ToInt32(Console.ReadLine());
+//        Array statusy = Enum.GetValues(typeof(StatusZamowienia));
+//        StatusZamowienia zaktualizowanyStatus = (StatusZamowienia)statusy.GetValue(wybrana - 1);
 
-        zamowienia[wybrane] = (zaktualizowanyStatus, zamowienia[wybrane].Item2);
-        Console.WriteLine(zamowienia[wybrane].Item1);
+//        zamowienia[wybrane] = (zaktualizowanyStatus, zamowienia[wybrane].Item2);
+//        Console.WriteLine(zamowienia[wybrane].Item1);
 
-    } 
-    catch (KeyNotFoundException ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-    catch (ArgumentException ex)
-    {
-        Console.WriteLine(ex.Message );
-    }
+//    } 
+//    catch (KeyNotFoundException ex)
+//    {
+//        Console.WriteLine(ex.Message);
+//    }
+//    catch (ArgumentException ex)
+//    {
+//        Console.WriteLine(ex.Message );
+//    }
    
-}
+//}
 
 
-static void ShowOrders(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
+//static void ShowOrders(Dictionary<int, (StatusZamowienia, List<string>)> zamowienia)
+//{
+//    foreach (var przedmiot in zamowienia)
+//    {
+//        Console.WriteLine($"Id: {przedmiot.Key}");
+//        Console.WriteLine($"Status zamowienia: {przedmiot.Value.Item1}");
+//        Console.WriteLine("Przedmioty:");
+//        foreach (var fruit in przedmiot.Value.Item2) 
+//        {
+//            Console.WriteLine($"- {fruit}");
+//        }
+//    }
+//}
+//ShowOrders(zamowienia);
+//ChangeStatus(zamowienia);
+
+//zad 3
+
+static void ZgadywanieKoloru()
 {
-    foreach (var przedmiot in zamowienia)
+    List<Kolory> list = new List<Kolory>() {Kolory.Czerwony,Kolory.Niebieski,Kolory.Zielony,Kolory.Żółty,Kolory.Fioletowy };
+    Random rand = new Random();
+    int randomIndex = rand.Next(0, list.Count);
+    Kolory wylosowany = list[rand.Next(list.Count)];
+    Console.WriteLine(wylosowany.ToString());
+
+    Console.WriteLine("Wybierz kolor z dostępnych na w tej liście");
+    foreach (var kolory in list)
     {
-        Console.WriteLine($"Id: {przedmiot.Key}");
-        Console.WriteLine($"Status zamowienia: {przedmiot.Value.Item1}");
-        Console.WriteLine("Przedmioty:");
-        foreach (var fruit in przedmiot.Value.Item2) 
+        Console.WriteLine(kolory.ToString() );
+    }
+    
+    bool sukces = false;
+
+    
+        while (sukces == false) 
         {
-            Console.WriteLine($"- {fruit}");
+            string podana = Console.ReadLine();
+            Kolory podanyKolor = (Kolory)Enum.Parse(typeof(Kolory), podana, true);
+        try
+        {
+            if (podanyKolor == wylosowany)
+            {
+                Console.WriteLine("Zgadles");
+                sukces = true;
+            }
+            else
+            {
+                Console.WriteLine("Sprobuj ponownie");
+            }
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Nie ma takiego koloru w dostepnych");
         }
     }
-}
-ShowOrders(zamowienia);
-ChangeStatus(zamowienia);
+       }
+ZgadywanieKoloru();
